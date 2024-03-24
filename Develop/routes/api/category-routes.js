d4e6a0e56815f31e4cb531bbd99535e7322dd0a8
Michael Chiem <michaelchiem1@hotmail.com>
2024-03-24 10:48:11 -0600

    Update Categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,22 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  Category.findAll().then(dbCategoryData => res.json(dbCategoryData));
-});
-
-router.get('/:id', async (req, res) => {
-  const pk = req.params.id;
-  const data = await Category.findByPk(pk, 
-    {
-      include:[{model: Product}]
-    }
-  );
-
-
-  res.json(data);
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
